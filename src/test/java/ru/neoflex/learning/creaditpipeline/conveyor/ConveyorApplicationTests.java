ed03dab36868989a1b5c328b5ed394d56379eb60
Aleksandra Zobnina <aozobnina@vtb.ru>
2023-09-13 14:04:01 +0300

    init

---
+++ src/test/java/ru/neoflex/learning/creaditpipeline/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.learning.creaditpipeline.conveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
